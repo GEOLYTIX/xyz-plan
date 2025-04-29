8989a753baa89914b694e6842fccdd966774c8db
Robert Hurst <robert.hurst@geolytix.co.uk>
2025-04-29 08:07:17 +0200

    disable clean url

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   description: 'A project application to show our progress',
   head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   lastUpdated: true,
-  cleanUrls: true,
+  cleanUrls: false,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: {
